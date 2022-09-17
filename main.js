3144cb44fdf9c3a8c3c75d1f7872828e7b2d9443
Rab Beverly <rab@robertbeverly.com>
2022-09-17 10:44:31 -0400

    fixed that for you rick

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 function show_modal(id) {
+    if (id === "12main") {
+        drop_it(id);
+    }
     document.getElementById(id).style.display = "block";
 }
 
 function hide_modal(id) {
     document.getElementById(id).style.display = "none";
+}
+
+function drop_it(e) {
+    // document.getElementById('playa').play();
+    // document.getElementById('playa').muted = false;
+    // document.getElementById('playa').play();
+    document.getElementById('playa').muted = false;
+    document.getElementById('playa').play();
+}
+
+function shhh(e) {
+    if (e.key === 's') {
+        console.log('s pressed');
+        document.getElementById('playa').pause();
+    }
 }
